7c091ce7740e2773442288b10e1d0077210ae84b
DrouetDanilo <ddrouet@espol.edu.ec>
2025-06-25 18:53:39 +0000

    guia 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,47 @@
-import React from 'react';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import { Grid } from '@mui/material';
+import './App.css'
+import HeaderUI from '/workspaces/dashboard/src/assets/components/HeaderUI.tsx';
+import AlertUI from '/workspaces/dashboard/src/assets/components/AlertUI.tsx';
 
 function App() {
-   return (
-      <Grid
-         container
-         spacing={5}
-         justifyContent="center"
-         alignItems="center"
-      >
-         {/* Encabezado */}
-         <Grid item xs={12} md={12}>
-            Elemento: Encabezado
-         </Grid>
-
-         {/* Alertas */}
-         <Grid item xs={12} md={12}>
-            Elemento: Alertas
-         </Grid>
-
-         {/* Selector */}
-         <Grid item xs={12} md={3}>
-            Elemento: Selector
-         </Grid>
-
-         {/* Indicadores */}
-         <Grid item xs={12} md={9}>
-            Elemento: Indicadores
-         </Grid>
-
-         {/* Gráfico */}
-         <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{
-               display: { xs: 'none', md: 'block' }
-            }}
-         >
-            Elemento: Gráfico
-         </Grid>
-
-         {/* Tabla */}
-         <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{
-               display: { xs: 'none', md: 'block' }
-            }}
-         >
-            Elemento: Tabla
-         </Grid>
-
-         {/* Información adicional */}
-         <Grid item xs={12} md={12}>
-            Elemento: Información adicional
-         </Grid>
+  return (
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+      {/* Encabezado */}
+      <Grid size={{ xs: 12, md: 12 }}>
+        <HeaderUI />
       </Grid>
-   );
+      
+      {/* Alertas */}
+      <Grid size={{ xs: 12 }} container justifyContent="right" alignItems="center">
+        <AlertUI 
+          description="No se preveen lluvias"
+          variant="filled"
+          severity="info"
+        />
+      </Grid>
+      
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+      
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+      
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, md: 6 }}
+        sx={{ display: { xs: "none", md: "block"} }}>
+        Elemento: Gráfico
+      </Grid>
+
+      {/* Tabla */}
+      <Grid size={{ xs: 12, md: 6 }}
+        sx={{ display: { xs: "none", md: "block"} }}>
+        Elemento: Tabla
+      </Grid>
+      
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+    </Grid>
+  );
 }
 
 export default App;
